e9132b93d43131889abe8bc7247846d577536347
Michal Svačina <msvacina@outlook.com>
2021-03-25 11:07:14 +0100

    Redis connection implemented

--- src/routers/redis.py
+++ src/routers/redis.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import redis
 
 router = APIRouter(
     prefix="/api/redis",
@@ -6,6 +7,18 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+r = redis.Redis(
+    host='192.168.1.27',
+    port=6379
+)
+
+
 @router.get("/")
 async def read_redis_root():
     return {"message": "Welcome to Redis API"}
+
+
+@router.get('/{key}')
+async def read_redis_key(key: str):
+    value = r.get(key)
+    return {'Key': key, 'Value': value}
