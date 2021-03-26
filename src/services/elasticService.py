90bf50a1b1b6ad8e0ac8b21e094a96168ece6ae0
Michal Svačina <msvacina@outlook.com>
2021-03-26 13:10:36 +0100

    Elastic service upgrade

--- src/services/elasticService.py
+++ src/services/elasticService.py
@@ -1,6 +1,8 @@
 from elasticsearch import Elasticsearch
 from datetime import datetime
 from services import configurationService
+import json
+
 
 class ElasticService:
     def __init__(self):
@@ -8,4 +10,34 @@ class ElasticService:
         self.es = Elasticsearch(self.elasticHost)
 
     def indexDocument(self, document):
+        # document.update('timestamp': datetime.now())
         self.es.index(index='python-api-logs', body=document)
+
+    def indexRequest(self, method: str, url: str, status_code: int, headers: {}, body: str = None):
+        document = self.__createDocument__(body, method, url, status_code)
+
+        self.__add_headers__(document, headers)
+        document.update({'timestamp': datetime.now()})
+        self.es.index(index='python-api-logs', body=document)
+
+    def __createDocument__(self, body, method, url, status_code):
+        if (body == None):
+            document = {
+                'method': method,
+                'url': url,
+                'status_code': status_code
+            }
+        elif (body != None):
+            document = {
+                'method': method,
+                'url': url,
+                'status_code': status_code,
+                'body': body
+            }
+        return document
+
+    def __add_headers__(self, document, headers):
+        for header in headers:
+            document.update({f'header.{header}': headers.get(header)})
+
+        return document
