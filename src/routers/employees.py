b939450de921330bc1e008dfbc72ddf972c16d67
Michal Svačina <msvacina@outlook.com>
2021-03-28 12:47:58 +0200

    Table creation in employees done. Get employee too

--- src/routers/employees.py
+++ src/routers/employees.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
-from services.employeeService import EmployeeService
 import psycopg2
+from fastapi import APIRouter
+from pydantic import BaseModel
+from services.employeeService import EmployeeService
+from typing import Optional
 
 router = APIRouter(
     prefix="/api/employees",
@@ -8,9 +10,20 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+class Employee(BaseModel):
+    id: Optional[int]
+    name: str
+    position: str
+    managerId: Optional[int]
+    salary: int
+
 employees = EmployeeService()
 
 @router.get("/")
 async def read_employees_root():
     pgStatus = employees.get_postgres_status()
     return {"message": "Welcome to employees API", 'isPostgresUp': pgStatus}
+
+@router.post('/new')
+async def post_new_employee(employee: Employee):
+    return employee
--- src/services/employeeService.py
+++ src/services/employeeService.py
@@ -1,13 +1,32 @@
 import psycopg2
 
+from services import configurationService
+
+
 class EmployeeService:
     def __init__(self):
         try:
-            self.connection_string = f'host=postgredb.msvacina.cz port=5432 dbname=python-rest user=python-rest_app password=Pythonheslo'
+            host = configurationService.config['PostgreSQL']['Host']
+            port = configurationService.config['PostgreSQL']['Port']
+            db = configurationService.config['PostgreSQL']['Database']
+            self.user = configurationService.config['PostgreSQL']['User']
+            password = configurationService.config['PostgreSQL']['Password']
+            self.schema = configurationService.config['PostgreSQL']['Schema']
+            self.table = configurationService.config['PostgreSQL']['Tables']['Employees']
+            self.connection_string = f'host={host} port={port} dbname={db} user={self.user} password={password}'
             self.connection = psycopg2.connect(self.connection_string)
             self.cur = self.connection.cursor()
             self._alive = True
-        except:
+
+            if (not self.check_existing_table()):
+                self.create_table()
+                print('Table created')
+
+        except Exception as e:
+            if hasattr(e, 'message'):
+                print(e.message)
+            else:
+                print(e)
             self._alive = False
 
     def get_postgres_status(self):
@@ -30,3 +49,51 @@ class EmployeeService:
             self._alive = True
         except:
             self._alive = False
+
+    def check_existing_table(self):
+        sql = f'''SELECT true
+        FROM information_schema."tables"
+        WHERE table_schema = '{self.schema}'
+        AND table_name = '{self.table}';
+        '''
+        self.cur.execute(sql)
+        records = self.cur.fetchall()
+
+        try:
+            if (records[0][0] == True):
+                return True
+        except:
+            return False
+
+    def create_table(self):
+        sql = f'''CREATE TABLE {self.schema}.{self.table}
+        (
+            id BIGSERIAL PRIMARY KEY,
+            name character varying(64) NOT NULL,
+            position character varying(32) NOT NULL,
+            salary integer NOT NULL,
+            "managerId" integer
+        )
+
+        TABLESPACE pg_default;
+
+        ALTER TABLE {self.schema}.{self.table}
+            OWNER to "{self.user}";
+
+        COMMIT;'''
+        self.cur.execute(sql)
+
+    def get_employee(self, id: int):
+        self.cur.execute(
+            f'SELECT * from {self.schema}.{self.table} WHERE id = {id}')
+        result = self.cur.fetchall()
+        return result
+
+    def get_all_employees(self):
+        return None
+
+    def create_employee(self):
+        return None
+
+    def delete_employee(self):
+        return None
