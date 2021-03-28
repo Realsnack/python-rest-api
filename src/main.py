639e091e139629160dc67272dbd89d9175e95992
Michal Svačina <msvacina@outlook.com>
2021-03-28 10:49:01 +0200

    Health endpoint added

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ from starlette.middleware.base import BaseHTTPMiddleware
 
 from middlewares import processTimeHeader, requestLogging
 from routers import employees, redis
+from services import redisService
+from services.employeeService import EmployeeService
 
 app = FastAPI()
 
@@ -14,11 +16,20 @@ app.add_middleware(BaseHTTPMiddleware, dispatch=requestLogging.log_request)
 app.include_router(redis.router)
 app.include_router(employees.router)
 
+
 @app.get("/")
 async def read_root():
     return {"message": "Hello world!", 'Time': datetime.now()}
 
+
+@app.get('/health')
+async def read_health():
+    redis_status = redisService.get_redis_status()
+    employees = EmployeeService()
+    employee_status = employees.get_postgres_status()
+    return {'redis': redis_status, 'postgresql - employees': employee_status}
+
+
 @app.get("/{name}")
 async def read_root_with_name(name: str):
     return {"message": f"Hello {name}"}
-    
