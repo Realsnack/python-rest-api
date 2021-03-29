72f15d37889f067e4e10c10c699bacced40c76cb
Michal Svačina <msvacina@outlook.com>
2021-03-29 16:10:42 +0200

    Implemnted Elastic APM

--- src/main.py
+++ src/main.py
@@ -8,9 +8,16 @@ from middlewares import processTimeHeader, requestLogging
 from routers import employees, redis
 from services import redisService
 from services.employeeService import EmployeeService
+from elasticapm.contrib.starlette import make_apm_client, ElasticAPM
 
+apm = make_apm_client({
+    'SERVICE_NAME': 'python-rest-api',
+    'SECRET_TOKEN': '',
+    'SERVER_URL': 'http://ubuntu01.msvacina.cz:8200/'
+})
 app = FastAPI()
 
+app.add_middleware(ElasticAPM, client=apm)
 app.add_middleware(BaseHTTPMiddleware, dispatch=processTimeHeader.add_process_time_header)
 app.add_middleware(BaseHTTPMiddleware, dispatch=requestLogging.log_request)
 app.include_router(redis.router)
